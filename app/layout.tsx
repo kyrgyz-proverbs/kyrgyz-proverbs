94b89f1aec34f26706bd8e3fcdea6525cd8b9e56
kyrgyz-proverbs <lakapmakal@gmail.com>
2026-08-04 21:49:33 +0200

    Add AdSense verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Makalla Kap - Kırgız Atasözleri",
   description: "Kırgız atasözleri, anlamları ve Kırgız kültürü hakkında bilgiler.",
+  other: {
+    "google-adsense-account": "ca-pub-2856862174002222",
+  },
 };
 
 export default function RootLayout({
@@ -26,20 +29,21 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="tr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
 
         <Script
-  async
-  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2856862174002222"
-  crossOrigin="anonymous"
-/>
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2856862174002222"
+          crossOrigin="anonymous"
+        />
 
         {children}
 
         <Analytics />
+
       </body>
     </html>
   );
